0d146731b48e216e153c10b3e81ff39403df2b52
IdaSofieKarlsson <ida.sofie.karlsson@gmail.com>
2026-01-04 23:24:03 +0100

    AGAIN fixes to make testing work. Skip Firebase Admin init during tests

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,12 +1,20 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./firebaseServiceAccount.json", "utf8")
-);
+/*this fix makes that 
+In development → Firebase Admin initializes normally
+In production → Firebase Admin initializes normally
+In tests → Firebase Admin is NOT initialized
+No file read → no ENOENT → tests can run*/
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (process.env.NODE_ENV !== "test") {
+  const serviceAccount = JSON.parse(
+    fs.readFileSync("./firebaseServiceAccount.json", "utf8")
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
